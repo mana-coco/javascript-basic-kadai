be155ce37d2319869648c5fca22e287fe24d9fe5
mana-coco <mana.mcoco.c7@gmail.com>
2025-06-21 22:01:08 +0900

    kadai_007提出分

---
+++ kadai_007/calc.js
@@ -0,0 +1,25 @@
+//変数numにランダムな整数を代入//
+let num = Math.floor(Math.random()*16);
+
+//変数numの値を出力//
+console.log(num);
+
+//変数numの値が3と5の倍数の場合//
+if(num % 5 == 0 && num % 3 == 0) {
+    console.log('3と5の倍数です');
+}
+
+//変数numの値が3の倍数の場合//
+else if(num % 3 == 0) {
+    console.log('3の倍数です');
+}
+
+//変数numの値が5の倍数の場合//
+else if(num % 5 == 0) {
+    console.log('5の倍数です');
+}
+
+//変数numの値が上記以外の場合//
+else {
+    console.log(num);
+}
